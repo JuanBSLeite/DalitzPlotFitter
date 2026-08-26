698cb60b4e31a1da404c9c035f40a7ef081b83f9
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 15:12:48 -0300

    Export fit-aware coefficient sets

--- src/dalitzplotfitter/coefficients/__init__.py
+++ src/dalitzplotfitter/coefficients/__init__.py
@@ -1,6 +1,7 @@
 """Amplitude coefficient parameterisations."""
 
 from .base import Coefficient, Flavor
+from .fit import FitCartesianCP, FitMagPhase
 from .sets import (
     BelleCP,
     CartesianCP,
@@ -20,6 +21,8 @@ __all__ = [
     "CartesianGammaCP",
     "CleoCP",
     "Coefficient",
+    "FitCartesianCP",
+    "FitMagPhase",
     "Flavor",
     "MagPhase",
     "MagPhaseCP",
