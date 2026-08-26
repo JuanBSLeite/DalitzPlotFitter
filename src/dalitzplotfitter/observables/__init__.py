9102a8d8171b0cd5cf1a4c1709173d9c10eb7967
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 14:34:08 -0300

    Expose amplitude observables

---
+++ src/dalitzplotfitter/observables/__init__.py
@@ -0,0 +1,6 @@
+"""Derived amplitude-analysis observables."""
+
+from .cp import cp_asymmetry
+from .fractions import fit_fractions, interference_fractions
+
+__all__ = ["cp_asymmetry", "fit_fractions", "interference_fractions"]
