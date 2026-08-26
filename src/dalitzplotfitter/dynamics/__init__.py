895d3292a16969f6809b403091a1dc6dc038f7cb
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 16:48:51 -0300

    Export symbolic dynamics

---
+++ src/dalitzplotfitter/dynamics/__init__.py
@@ -0,0 +1,17 @@
+"""DalitzPlotFitter-owned symbolic dynamics."""
+
+from .laura import (
+    blatt_weisskopf_factor,
+    breakup_momentum,
+    energy_dependent_width,
+    kallen,
+    relativistic_breit_wigner,
+)
+
+__all__ = [
+    "blatt_weisskopf_factor",
+    "breakup_momentum",
+    "energy_dependent_width",
+    "kallen",
+    "relativistic_breit_wigner",
+]
