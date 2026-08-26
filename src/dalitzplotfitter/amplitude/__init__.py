8684d80dd2fe435e0bfcc10685d167c90b8ed5dd
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 14:43:42 -0300

    Export kinematic transformer

--- src/dalitzplotfitter/amplitude/__init__.py
+++ src/dalitzplotfitter/amplitude/__init__.py
@@ -1,5 +1,11 @@
 """Amplitude-model construction."""
 
 from .builder import AmplitudeBuilder, compile_model
+from .transform import KinematicTransformer, create_kinematic_transformer
 
-__all__ = ["AmplitudeBuilder", "compile_model"]
+__all__ = [
+    "AmplitudeBuilder",
+    "KinematicTransformer",
+    "compile_model",
+    "create_kinematic_transformer",
+]
