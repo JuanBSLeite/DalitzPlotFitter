9a2bbf113c0c96096f30ac7d6410e5e6b0497862
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 14:57:11 -0300

    Export coherent amplitude API

--- src/dalitzplotfitter/amplitude/__init__.py
+++ src/dalitzplotfitter/amplitude/__init__.py
@@ -1,13 +1,23 @@
 """Amplitude-model construction."""
 
 from .builder import AmplitudeBuilder, compile_model
+from .components import (
+    AmplitudeComponent,
+    CoherentAmplitudeModel,
+    ConstantAmplitude,
+    compile_amplitude_component,
+)
 from .model import CompiledModel
 from .transform import KinematicTransformer, create_kinematic_transformer
 
 __all__ = [
     "AmplitudeBuilder",
+    "AmplitudeComponent",
+    "CoherentAmplitudeModel",
     "CompiledModel",
+    "ConstantAmplitude",
     "KinematicTransformer",
+    "compile_amplitude_component",
     "compile_model",
     "create_kinematic_transformer",
 ]
