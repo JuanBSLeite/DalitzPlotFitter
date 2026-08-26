77223f75eddc3cf3d297874eb886b9ca52446e2f
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 14:32:17 -0300

    Expose efficiency models

---
+++ src/dalitzplotfitter/efficiency/__init__.py
@@ -0,0 +1,5 @@
+"""Efficiency models."""
+
+from .models import FunctionalEfficiency, HistogramEfficiency, UnityEfficiency
+
+__all__ = ["FunctionalEfficiency", "HistogramEfficiency", "UnityEfficiency"]
