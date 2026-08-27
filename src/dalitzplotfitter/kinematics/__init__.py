c45d70b0c04526652eb2920826e55bbc95619602
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:24:35 -0300

    Export covariant kinematics and phasespace MC

--- src/dalitzplotfitter/kinematics/__init__.py
+++ src/dalitzplotfitter/kinematics/__init__.py
@@ -1,5 +1,11 @@
 """Kinematic utilities."""
 
+from .covariant import (
+    CovariantKinematics,
+    boost_to_rest_frame,
+    covariant_kinematics,
+    spatial_magnitude,
+)
 from .dalitz import (
     inside_dalitz,
     invariant_sum,
@@ -10,10 +16,15 @@ from .dalitz import (
 )
 from .four_vectors import four_momenta_from_dalitz, invariant_mass_squared
 from .phase_space import PhaseSpaceSample, ThreeBodyPhaseSpace
+from .phasespace_mc import PhasespaceMC
 
 __all__ = [
+    "CovariantKinematics",
     "PhaseSpaceSample",
+    "PhasespaceMC",
     "ThreeBodyPhaseSpace",
+    "boost_to_rest_frame",
+    "covariant_kinematics",
     "four_momenta_from_dalitz",
     "inside_dalitz",
     "invariant_mass_squared",
@@ -22,4 +33,5 @@ __all__ = [
     "s12_limits",
     "s13_from_s12_s23",
     "s23_limits",
+    "spatial_magnitude",
 ]
