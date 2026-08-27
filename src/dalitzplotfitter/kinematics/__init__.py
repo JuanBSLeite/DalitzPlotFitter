5aedf57b2867bf48afb6c79413c1e12b2cda3638
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:34:50 -0300

    Remove legacy JAX phase-space exports

--- src/dalitzplotfitter/kinematics/__init__.py
+++ src/dalitzplotfitter/kinematics/__init__.py
@@ -1,4 +1,4 @@
-"""Kinematic utilities."""
+"""Kinematic utilities for Laura++-style amplitudes."""
 
 from .covariant import (
     CovariantKinematics,
@@ -6,32 +6,16 @@ from .covariant import (
     covariant_kinematics,
     spatial_magnitude,
 )
-from .dalitz import (
-    inside_dalitz,
-    invariant_sum,
-    kallen,
-    s12_limits,
-    s13_from_s12_s23,
-    s23_limits,
-)
-from .four_vectors import four_momenta_from_dalitz, invariant_mass_squared
-from .phase_space import PhaseSpaceSample, ThreeBodyPhaseSpace
 from .phasespace_mc import PhasespaceMC
+from .sample import PhaseSpaceSample
+from .vectors import invariant_mass_squared
 
 __all__ = [
     "CovariantKinematics",
     "PhaseSpaceSample",
     "PhasespaceMC",
-    "ThreeBodyPhaseSpace",
     "boost_to_rest_frame",
     "covariant_kinematics",
-    "four_momenta_from_dalitz",
-    "inside_dalitz",
     "invariant_mass_squared",
-    "invariant_sum",
-    "kallen",
-    "s12_limits",
-    "s13_from_s12_s23",
-    "s23_limits",
     "spatial_magnitude",
 ]
