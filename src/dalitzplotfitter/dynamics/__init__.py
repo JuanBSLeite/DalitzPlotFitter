b278206533ad75066e6cfde24039bb4011561163
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:26:22 -0300

    Export numerical Laura++ covariant component

--- src/dalitzplotfitter/dynamics/__init__.py
+++ src/dalitzplotfitter/dynamics/__init__.py
@@ -1,6 +1,11 @@
-"""DalitzPlotFitter-owned symbolic dynamics."""
+"""DalitzPlotFitter-owned Laura++-style dynamics."""
 
 from .builder import LauraRelativisticBreitWignerBuilder
+from .covariant import (
+    LauraCovariantRBW,
+    blatt_weisskopf_from_momenta,
+    covariant_spin_factor,
+)
 from .laura import (
     bachelor_momentum_parent_frame,
     bachelor_momentum_resonance_frame,
@@ -13,12 +18,15 @@ from .laura import (
 )
 
 __all__ = [
+    "LauraCovariantRBW",
     "LauraRelativisticBreitWignerBuilder",
     "bachelor_momentum_parent_frame",
     "bachelor_momentum_resonance_frame",
     "blatt_weisskopf_factor",
+    "blatt_weisskopf_from_momenta",
     "breakup_momentum",
     "covariant_angular_factor",
+    "covariant_spin_factor",
     "energy_dependent_width",
     "kallen",
     "relativistic_breit_wigner",
