e79b5c81e0dea36a88bea6ac29e43a630a6dc71b
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:36:19 -0300

    Remove legacy coefficient APIs

--- src/dalitzplotfitter/coefficients/__init__.py
+++ src/dalitzplotfitter/coefficients/__init__.py
@@ -1,33 +1,5 @@
-"""Amplitude coefficient parameterisations."""
+"""Amplitude coefficient parameterization."""
 
-from .base import Coefficient, Flavor
-from .fit import FitCartesianCP, FitMagPhase
-from .sets import (
-    BelleCP,
-    CartesianCP,
-    CartesianGammaCP,
-    CleoCP,
-    MagPhase,
-    MagPhaseCP,
-    PolarGammaCP,
-    RealImag,
-    RealImagCP,
-    RealImagGammaCP,
-)
+from .sets import RealImag
 
-__all__ = [
-    "BelleCP",
-    "CartesianCP",
-    "CartesianGammaCP",
-    "CleoCP",
-    "Coefficient",
-    "FitCartesianCP",
-    "FitMagPhase",
-    "Flavor",
-    "MagPhase",
-    "MagPhaseCP",
-    "PolarGammaCP",
-    "RealImag",
-    "RealImagCP",
-    "RealImagGammaCP",
-]
+__all__ = ["RealImag"]
