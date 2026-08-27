b884907e0c3002492d323f2bff9db180b399c585
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:56:17 -0300

    Export Bose-symmetrized amplitude wrapper

--- src/dalitzplotfitter/amplitude/__init__.py
+++ src/dalitzplotfitter/amplitude/__init__.py
@@ -1,10 +1,16 @@
 """Amplitude-model utilities."""
 
 from .cache import PreparedAmplitudeCache
-from .components import AmplitudeComponent, CoherentAmplitudeModel, ConstantAmplitude
+from .components import (
+    AmplitudeComponent,
+    BoseSymmetrizedAmplitude,
+    CoherentAmplitudeModel,
+    ConstantAmplitude,
+)
 
 __all__ = [
     "AmplitudeComponent",
+    "BoseSymmetrizedAmplitude",
     "CoherentAmplitudeModel",
     "ConstantAmplitude",
     "PreparedAmplitudeCache",
