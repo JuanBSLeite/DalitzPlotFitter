fcc0c3641e00c0bc1033b7337fb6e45bed599bd6
Juan Leite <juanleite.phd@gmail.com>
2026-08-26 22:19:20 -0300

    Export Laura++ covariant angular helpers

--- src/dalitzplotfitter/dynamics/__init__.py
+++ src/dalitzplotfitter/dynamics/__init__.py
@@ -2,8 +2,11 @@
 
 from .builder import LauraRelativisticBreitWignerBuilder
 from .laura import (
+    bachelor_momentum_parent_frame,
+    bachelor_momentum_resonance_frame,
     blatt_weisskopf_factor,
     breakup_momentum,
+    covariant_angular_factor,
     energy_dependent_width,
     kallen,
     relativistic_breit_wigner,
@@ -11,8 +14,11 @@ from .laura import (
 
 __all__ = [
     "LauraRelativisticBreitWignerBuilder",
+    "bachelor_momentum_parent_frame",
+    "bachelor_momentum_resonance_frame",
     "blatt_weisskopf_factor",
     "breakup_momentum",
+    "covariant_angular_factor",
     "energy_dependent_width",
     "kallen",
     "relativistic_breit_wigner",
