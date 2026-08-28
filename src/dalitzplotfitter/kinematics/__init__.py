96e00c9bb5b28fb54796904687c2005fa31bfc4a
Juan Leite <juanleite.phd@gmail.com>
2026-08-27 23:56:18 -0300

    Export Dalitz grid integration utilities

--- src/dalitzplotfitter/kinematics/__init__.py
+++ src/dalitzplotfitter/kinematics/__init__.py
@@ -7,17 +7,20 @@ from .covariant import (
     covariant_kinematics_from_invariants,
     spatial_magnitude,
 )
+from .dalitz_grid import DalitzGrid, dalitz_s13_limits
 from .phase_space_mc import PhaseSpaceMC
 from .sample import PhaseSpaceSample
 from .vectors import invariant_mass_squared
 
 __all__ = [
     "CovariantKinematics",
+    "DalitzGrid",
     "PhaseSpaceMC",
     "PhaseSpaceSample",
     "boost_to_rest_frame",
     "covariant_kinematics",
     "covariant_kinematics_from_invariants",
+    "dalitz_s13_limits",
     "invariant_mass_squared",
     "spatial_magnitude",
 ]
