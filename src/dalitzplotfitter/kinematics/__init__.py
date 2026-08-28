d303e8aa15ef5a465208a1e1d1cf2742363eac04
Juan Leite <juanleite.phd@gmail.com>
2026-08-27 22:30:50 -0300

    Export pure-JAX PhaseSpaceMC

--- src/dalitzplotfitter/kinematics/__init__.py
+++ src/dalitzplotfitter/kinematics/__init__.py
@@ -7,14 +7,14 @@ from .covariant import (
     covariant_kinematics_from_invariants,
     spatial_magnitude,
 )
-from .phasespace_mc import PhasespaceMC
+from .phase_space_mc import PhaseSpaceMC
 from .sample import PhaseSpaceSample
 from .vectors import invariant_mass_squared
 
 __all__ = [
     "CovariantKinematics",
+    "PhaseSpaceMC",
     "PhaseSpaceSample",
-    "PhasespaceMC",
     "boost_to_rest_frame",
     "covariant_kinematics",
     "covariant_kinematics_from_invariants",
