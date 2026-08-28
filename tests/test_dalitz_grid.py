987187bbd738962262d025a566f5cf1c53bb324c
Juan Leite <juanleite.phd@gmail.com>
2026-08-28 00:17:31 -0300

    Test equal-area Dalitz contour grid

--- tests/test_dalitz_grid.py
+++ tests/test_dalitz_grid.py
@@ -8,21 +8,25 @@ from dalitzplotfitter import DalitzGrid, DecayChannel, dalitz_s13_limits, enable
 enable_x64()
 
 
-def test_dalitz_grid_contains_only_physical_midpoints():
+def test_dalitz_grid_contains_exactly_n_squared_physical_midpoints():
     channel = DecayChannel("D+", ("pi-", "pi+", "pi+"))
-    sample = DalitzGrid(
+    resolution = 120
+    grid = DalitzGrid(
         channel.parent_mass,
         channel.daughter_masses,
-        resolution=120,
-    ).sample()
+        resolution=resolution,
+    )
+    sample = grid.sample()
+
+    assert sample.size == resolution**2
 
     low, high = dalitz_s13_limits(
         sample.s12,
         mother_mass=channel.parent_mass,
         masses=channel.daughter_masses,
     )
-    assert bool(jnp.all(sample.s13 >= low))
-    assert bool(jnp.all(sample.s13 <= high))
+    assert bool(jnp.all(sample.s13 > low))
+    assert bool(jnp.all(sample.s13 < high))
 
     m1, m2, m3 = channel.daughter_masses
     invariant_sum = channel.parent_mass**2 + m1**2 + m2**2 + m3**2
@@ -34,49 +38,60 @@ def test_dalitz_grid_contains_only_physical_midpoints():
     )
 
 
-def test_dalitz_grid_uses_constant_midpoint_weights():
+def test_dalitz_grid_uses_constant_equal_area_weight():
     channel = DecayChannel("D+", ("pi-", "pi+", "pi+"))
-    resolution = 100
-    sample = DalitzGrid(
+    grid = DalitzGrid(
         channel.parent_mass,
         channel.daughter_masses,
-        resolution=resolution,
-    ).sample()
+        resolution=100,
+    )
+    sample = grid.sample()
 
-    assert sample.size > 0
     assert bool(jnp.all(sample.weights == sample.weights[0]))
-
-    m1, m2, m3 = channel.daughter_masses
-    ds12 = ((channel.parent_mass - m3) ** 2 - (m1 + m2) ** 2) / resolution
-    ds13 = ((channel.parent_mass - m2) ** 2 - (m1 + m3) ** 2) / resolution
-    expected_area = sample.size * ds12 * ds13
-
-    # The package integration convention is mean(weights * f). For f=1 this
-    # must exactly reproduce the midpoint quadrature cell_area * N_valid.
     estimated_area = float(jnp.mean(sample.weights))
-    assert math.isclose(estimated_area, expected_area, rel_tol=2e-14, abs_tol=2e-14)
+    assert math.isclose(
+        estimated_area,
+        float(grid.area),
+        rel_tol=2e-14,
+        abs_tol=2e-14,
+    )
 
 
-def test_dalitz_grid_mean_estimator_equals_midpoint_sum():
+def test_dalitz_grid_has_equal_area_s12_strips():
     channel = DecayChannel("D+", ("pi-", "pi+", "pi+"))
-    resolution = 80
-    sample = DalitzGrid(
+    resolution = 64
+    grid = DalitzGrid(
         channel.parent_mass,
         channel.daughter_masses,
         resolution=resolution,
-    ).sample()
+        boundary_resolution=20001,
+    )
+    sample = grid.sample()
+
+    # Each s12 strip is repeated exactly N times and corresponds to an equal
+    # fraction of cumulative Dalitz area by construction.
+    strip_s12 = sample.s12.reshape(resolution, resolution)[:, 0]
+    assert bool(jnp.all(jnp.diff(strip_s12) > 0.0))
+    assert jnp.unique(sample.s12).shape[0] == resolution
+    assert sample.s13.reshape(resolution, resolution).shape == (resolution, resolution)
+
+
+def test_dalitz_grid_mean_estimator_is_area_times_mean():
+    channel = DecayChannel("D+", ("pi-", "pi+", "pi+"))
+    grid = DalitzGrid(
+        channel.parent_mass,
+        channel.daughter_masses,
+        resolution=80,
+    )
+    sample = grid.sample()
 
     values = 1.0 + 0.3 * sample.s12 + 0.2 * sample.s13
     estimate_from_package_convention = jnp.mean(sample.weights * values)
-
-    m1, m2, m3 = channel.daughter_masses
-    ds12 = ((channel.parent_mass - m3) ** 2 - (m1 + m2) ** 2) / resolution
-    ds13 = ((channel.parent_mass - m2) ** 2 - (m1 + m3) ** 2) / resolution
-    direct_midpoint_sum = ds12 * ds13 * jnp.sum(values)
+    direct_equal_area_quadrature = grid.area * jnp.mean(values)
 
     assert jnp.allclose(
         estimate_from_package_convention,
-        direct_midpoint_sum,
+        direct_equal_area_quadrature,
         rtol=2e-14,
         atol=2e-14,
     )
