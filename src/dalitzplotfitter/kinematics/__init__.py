4115d1bac087ce9d343bd4528fa4010a7ee932c1
Juan Leite <juanleite.phd@gmail.com>
2026-08-27 22:04:21 -0300

    Export invariant covariant kinematics helper

--- src/dalitzplotfitter/kinematics/__init__.py
+++ src/dalitzplotfitter/kinematics/__init__.py
@@ -1,9 +1,10 @@
-"""Kinematic utilities for Laura++-style amplitudes."""
+"""Kinematic utilities for three-body amplitudes."""
 
 from .covariant import (
     CovariantKinematics,
     boost_to_rest_frame,
     covariant_kinematics,
+    covariant_kinematics_from_invariants,
     spatial_magnitude,
 )
 from .phasespace_mc import PhasespaceMC
@@ -16,6 +17,7 @@ __all__ = [
     "PhasespaceMC",
     "boost_to_rest_frame",
     "covariant_kinematics",
+    "covariant_kinematics_from_invariants",
     "invariant_mass_squared",
     "spatial_magnitude",
 ]
