b88df2669f4d34beeaa878b5ff45320cf1e91a29
Juan Leite <juanleite.phd@gmail.com>
2026-08-29 22:08:56 -0300

    Export deterministic grid integrator only

--- src/dalitzplotfitter/integration/__init__.py
+++ src/dalitzplotfitter/integration/__init__.py
@@ -1,6 +1,6 @@
 """Numerical normalization tools."""
 
+from .grid import GridIntegrator
 from .matrix import matrix_normalization, normalization_matrix
-from .monte_carlo import MonteCarloIntegrator
 
-__all__ = ["MonteCarloIntegrator", "matrix_normalization", "normalization_matrix"]
+__all__ = ["GridIntegrator", "matrix_normalization", "normalization_matrix"]
