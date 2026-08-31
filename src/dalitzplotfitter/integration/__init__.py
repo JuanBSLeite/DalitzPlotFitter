959ee426e91a5937f8db96e301effbecfc256132
Juan Leite <juanleite.phd@gmail.com>
2026-08-31 14:45:21 -0300

    Expose adaptive Square Dalitz integration

--- src/dalitzplotfitter/integration/__init__.py
+++ src/dalitzplotfitter/integration/__init__.py
@@ -1,6 +1,13 @@
 """Numerical normalization tools."""
 
+from .adaptive_square import AdaptiveSquareDalitzGrid, AdaptiveSquareDalitzResult
 from .grid import GridIntegrator
 from .matrix import matrix_normalization, normalization_matrix
 
-__all__ = ["GridIntegrator", "matrix_normalization", "normalization_matrix"]
+__all__ = [
+    "AdaptiveSquareDalitzGrid",
+    "AdaptiveSquareDalitzResult",
+    "GridIntegrator",
+    "matrix_normalization",
+    "normalization_matrix",
+]
