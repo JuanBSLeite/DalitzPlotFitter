dde6c9ebe1e4137495d79d8a0a327fdac00e3dcd
Juan Leite <juanleite.phd@gmail.com>
2026-08-31 16:17:26 -0300

    Expose GenFit study API

--- src/dalitzplotfitter/fit/__init__.py
+++ src/dalitzplotfitter/fit/__init__.py
@@ -1,6 +1,16 @@
 """Fit configuration and minimization."""
 
+from .genfit import GaussianFitResult, GenFit, GenFitRecord, GenFitResult
 from .minimizer import Minimizer, MultiStartResult
 from .parameters import Parameter, ParameterKind
 
-__all__ = ["Minimizer", "MultiStartResult", "Parameter", "ParameterKind"]
+__all__ = [
+    "GaussianFitResult",
+    "GenFit",
+    "GenFitRecord",
+    "GenFitResult",
+    "Minimizer",
+    "MultiStartResult",
+    "Parameter",
+    "ParameterKind",
+]
