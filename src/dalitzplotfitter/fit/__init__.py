ffef7e3b5200148fe0cfb462c16985582cdb05b4
Juan Leite <juanleite.phd@gmail.com>
2026-08-31 16:28:45 -0300

    Expose GenFit outlier utilities

--- src/dalitzplotfitter/fit/__init__.py
+++ src/dalitzplotfitter/fit/__init__.py
@@ -2,6 +2,16 @@
 
 from .genfit import GaussianFitResult, GenFit, GenFitRecord, GenFitResult
 from .minimizer import Minimizer, MultiStartResult
+from .outliers import (
+    OutlierSelection,
+    RobustGaussianFitResult,
+    genfit_distribution,
+    genfit_outlier_selection,
+    genfit_robust_gaussian_fit,
+    genfit_robust_summary,
+    robust_gaussian_fit,
+    robust_outlier_mask,
+)
 from .parameters import Parameter, ParameterKind
 
 __all__ = [
@@ -11,6 +21,14 @@ __all__ = [
     "GenFitResult",
     "Minimizer",
     "MultiStartResult",
+    "OutlierSelection",
     "Parameter",
     "ParameterKind",
+    "RobustGaussianFitResult",
+    "genfit_distribution",
+    "genfit_outlier_selection",
+    "genfit_robust_gaussian_fit",
+    "genfit_robust_summary",
+    "robust_gaussian_fit",
+    "robust_outlier_mask",
 ]
