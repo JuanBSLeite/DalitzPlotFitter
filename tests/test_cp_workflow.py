9fb7ecca9d75c212021c8d077da3ba2746814999
Juan Leite <juanleite.phd@gmail.com>
2026-09-01 19:33:34 -0300

    Test CP projection joint normalization

--- tests/test_cp_workflow.py
+++ tests/test_cp_workflow.py
@@ -83,3 +83,13 @@ def test_cp_fit_session_symmetric_efficiency_is_convenient():
     assert session.minus_efficiency is efficiency
     assert jnp.allclose(session.plus_acceptance_data, 0.7)
     assert jnp.allclose(session.minus_acceptance_data, 0.7)
+
+
+def test_cp_projection_weights_preserve_joint_event_count():
+    plus, minus = _models()
+    session = CPFitSession(plus, minus, _data(), _data(0.02))
+    values = {"NR.x": 1.0, "NR.dx": 0.1}
+    plus_components = session._projection_components(values, "plus")
+    minus_components = session._projection_components(values, "minus")
+    total = jnp.sum(jnp.asarray(plus_components[0][2])) + jnp.sum(jnp.asarray(minus_components[0][2]))
+    assert jnp.allclose(total, session.plus_data.size + session.minus_data.size, rtol=1e-6)
