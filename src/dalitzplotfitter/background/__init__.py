e0bb5efadfb2699f40e03b58e90e8a9976b477e3
Juan Leite <juanleite.phd@gmail.com>
2026-09-01 18:19:45 -0300

    Export background categories

--- src/dalitzplotfitter/background/__init__.py
+++ src/dalitzplotfitter/background/__init__.py
@@ -1,5 +1,6 @@
-"""Background models."""
+"""Background models and named fit categories."""
 
+from .categories import BackgroundCategory
 from .models import FunctionalBackground, HistogramBackground
 
-__all__ = ["FunctionalBackground", "HistogramBackground"]
+__all__ = ["BackgroundCategory", "FunctionalBackground", "HistogramBackground"]
