dcfef6c4184ab22f7d8afcb289467042067f65ba
Juan Leite <juanleite.phd@gmail.com>
2026-09-03 00:20:30 -0300

    Export GooFit compatibility lineshapes

--- src/dalitzplotfitter/dynamics/lineshape/__init__.py
+++ src/dalitzplotfitter/dynamics/lineshape/__init__.py
@@ -2,6 +2,7 @@
 
 from .babar_flatte import BaBarFlatte
 from .common import (
+    bachelor_momentum_parent_frame,
     bachelor_momentum_resonance_frame,
     blatt_weisskopf_from_momenta,
     breakup_momentum,
@@ -10,7 +11,7 @@ from .common import (
     kallen,
 )
 from .flatte import Flatte
-from .gounaris_sakurai import GounarisSakurai
+from .gounaris_sakurai import GooFitLegacyGounarisSakurai, GounarisSakurai
 from .kmatrix import KMatrix
 from .lass import LASS
 from .pole import Pole
@@ -20,12 +21,14 @@ from .relativistic_breit_wigner import RelativisticBreitWigner
 __all__ = [
     "BaBarFlatte",
     "Flatte",
+    "GooFitLegacyGounarisSakurai",
     "GounarisSakurai",
     "KMatrix",
     "LASS",
     "Pole",
     "QMI",
     "RelativisticBreitWigner",
+    "bachelor_momentum_parent_frame",
     "bachelor_momentum_resonance_frame",
     "blatt_weisskopf_from_momenta",
     "breakup_momentum",
