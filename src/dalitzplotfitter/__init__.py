a1ab466b99c3be8fb4117600fde785f7f9f96760
Juan Leite <juanbsleite@proton.me>
2026-09-14 20:28:27 -0300

    TKDTree binning

--- src/dalitzplotfitter/__init__.py
+++ src/dalitzplotfitter/__init__.py
@@ -69,6 +69,7 @@ from .goodness_of_fit import (
     BinnedChi2Result,
     PointToPointResult,
     chi2_from_histograms,
+    kdtree_local_residuals,
     point_to_point_dissimilarity,
 )
 from .integration import DalitzGaussLegendreGrid
@@ -139,7 +140,7 @@ from .veto import (
 from .workflow import BackgroundSpec, FitSession
 
 __all__ = [
-    "AmplitudeComponent", "BackgroundCategory", "BackgroundSpec", "BinnedChi2Result", "BreitWigner1D", "CPBackgroundCategory",
+    "AmplitudeComponent", "BackgroundCategory", "BackgroundSpec", "BinnedChi2Result", "BreitWigner1D", "CPBackgroundCategory", "kdtree_local_residuals",
     "CPBackgroundSpec", "CPFitSession", "CPJointNLL", "CPToyBackground", "BaBarFlatte", "CPRealImag",
     "CoherentAmplitudeModel", "CompositeVeto", "ConstrainedNLL", "ConstantAmplitude",
     "ConvolvedPDF1D", "CovariantAngular", "CovariantKinematics", "DalitzAmplitude",
